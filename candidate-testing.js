6d19444088633c5552d3e7de5df7dc503d070a32
Stephen Bradshaw <ellisdee@Elliss-MacBook-Pro.local>
2024-06-23 11:03:52 -0500

    Candidate-Test-Autograded Task 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,20 +8,20 @@ let candidateName = "";
 let question = "Who was the first American woman in space? "
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-
-
+let answer = "";
+let grade = 0;
 
 
 
 //TODO: Variables for Part 2
 let questions = 
-  ["Who was the first American woman in space? ",
+  [question,
   "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "];
 
-let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', 3];
+let correctAnswers = [correctAnswer, "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -33,42 +33,38 @@ candidateName = input.question("What is your name? : ")
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(`${question}`);
-  if (candidateAnswer === NaN) {
-    console.log("You must enter a number. Please try again.")
-    candidateAnswer = input.question(`${question}`)
-  };
+  candidateAnswers.push(candidateAnswer)
 };
 
 function askQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    answer = input.question(`{questions[i]}`);
+  for (let i = 1; i < questions.length; i++) {
+    answer = input.question(questions[i]);
     candidateAnswers.push(answer)
   };
-
+console.log(candidateAnswers)
 };
+
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
-
-  if (candidateAnswer === "Sally Ride") {
-    console.log("You are correct. Sally Ride was the first woman in space.")
-    grade =+ 100
-  } else {
-    console.log("Incorrect. Sally Ride was the first woman in space.")
+ for ( let i = 0; i < questions.length; i++){
+  if (candidateAnswers[i] == correctAnswers[i]) {
+    grade = grade + 20
   }
+  console.log(`You entered ${candidateAnswers[i]}. The correct answer is ${correctAnswers[i]}`)
+ }
   console.log(`You scored ${grade} on the quiz.`)
   return grade;
   
-}
+} //
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log(`Hello, ${candidateName}`);
   askQuestion();
-  // askQuestions();
+  askQuestions();
   gradeQuiz(this.candidateAnswers);
 }
 
